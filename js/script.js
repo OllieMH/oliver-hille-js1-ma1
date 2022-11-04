5733cd24b03b16771c9ec2ac9415ede570b21e9a
OllieMH <olivermh99@hotmail.com>
2022-11-04 23:04:12 +0100

    just formatting

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
+	{
+		name: "Blob",
+		age: 10,
+	},
+	{
+		name: "Harold",
+	},
+	{
+		name: "Blurt",
+		age: 21,
+	},
 ];
 
 // question 1
 
 const cat = {
-    complain: function() {
-        console.log("Meow!");
-    }
+	complain: function () {
+		console.log("Meow!");
+	},
 };
 
 cat.complain();
@@ -38,8 +38,8 @@ heading.classList.add("subheading");
 // question 5
 
 const paragraphs = document.querySelectorAll("p");
-for(let i = 0; i < paragraphs.length; i++){
-    paragraphs[i].style.color = "red";
+for (let i = 0; i < paragraphs.length; i++) {
+	paragraphs[i].style.color = "red";
 }
 
 // question 6
@@ -50,27 +50,25 @@ resultsContainer.style.backgroundColor = "yellow";
 
 // question 7
 
-const listCats = function(list){
-    for(let i = 0; i < list.length; i++){
-        console.log(list[i].name);
-    }
-}
+const listCats = function (list) {
+	for (let i = 0; i < list.length; i++) {
+		console.log(list[i].name);
+	}
+};
 listCats(cats);
 
 // question 8
 
-const createCats = function(cats){
-    var html = ""
-    for(let i = 0; i < cats.length; i++){
-        if(cats[i].hasOwnProperty("age")){
-            html += "<div><h5>" + "Name: " + cats[i].name + "</h5><p>" + "Age: " + cats[i].age + "</p></div>"; // Assumed age and name tags are fine even though the task didn't mention them.
-        } else {
-            html += "<div><h5>" + "Name: " + cats[i].name + "</h5><p>" + "Age Unknown" + "</p></div>";
-        }
-    }
-    return html 
-}
+const createCats = function (cats) {
+	var html = "";
+	for (let i = 0; i < cats.length; i++) {
+		if (cats[i].hasOwnProperty("age")) {
+			html += "<div><h5>" + "Name: " + cats[i].name + "</h5><p>" + "Age: " + cats[i].age + "</p></div>"; // Assumed age and name tags are fine even though the task didn't mention them.
+		} else {
+			html += "<div><h5>" + "Name: " + cats[i].name + "</h5><p>" + "Age Unknown" + "</p></div>";
+		}
+	}
+	return html;
+};
 const catContainer = document.querySelector(".cat-container");
 catContainer.innerHTML = createCats(cats);
-
-
